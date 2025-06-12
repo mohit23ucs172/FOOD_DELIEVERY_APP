3ea68cd167c998e92809b99aad79b9580f53f41f
MOHIT TIWARI <163675793+mohit23ucs172@users.noreply.github.com>
2025-06-12 15:27:19 +0530

    add reactive interfaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'  
-import {Route,Routes} from 'react-router-dom'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
-import Home from './pages/Home/Home.jsx'
-import Cart from './pages/Cart/Cart.jsx'
-import Footer from './components/footer/Footer.jsx'
-import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import LoginPopup from "./components/LoginPopup/LoginPopup.jsx";
 const App = () => {
-
-  const [showLogin,setShowLogin] =useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-     <Navbar setShowLogin={setShowLogin}/>
-     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/cart' element={<Cart/>} />
-      <Route path='/order' element={<PlaceOrder/>} />
-     </Routes>
-    </div>
-    <Footer />
-      </>
-  )
-}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
